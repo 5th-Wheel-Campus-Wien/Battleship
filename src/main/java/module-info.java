e9c0b36ebe87be61f22ebd96a8fbd90f27a7d109
sarahhennenfeind <sarah.hennenfeind@stud.fh-campuswien.ac.at>
2025-01-11 16:49:09 +0100

    Hoffentlich funktionierts

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.battleship {
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens org.example.battleship to javafx.fxml;
     exports org.example.battleship;
